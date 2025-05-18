d41d022f7359624d887654f34c1995f642990fac
Eldar Kurbanov <EldarK@outlook.com>
2025-05-18 21:51:08 +0300

    Meeting 2

--- core/src/main/java/ru/innovationcampus/spacecleaner/Main.java
+++ core/src/main/java/ru/innovationcampus/spacecleaner/Main.java
@@ -1,17 +1,33 @@
 package ru.innovationcampus.spacecleaner;
 
+import static ru.innovationcampus.spacecleaner.GameSettings.POSITION_ITERATIONS;
+import static ru.innovationcampus.spacecleaner.GameSettings.STEP_TIME;
+import static ru.innovationcampus.spacecleaner.GameSettings.VELOCITY_ITERATIONS;
+
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+
 import ru.innovationcampus.spacecleaner.screens.GameScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     public SpriteBatch batch;
     public OrthographicCamera orthographicCamera;
+    public World world;
+    float accumulator = 0;
+    public Vector3 touch;
 
     @Override
     public void create() {
+        Box2D.init();
+        world = new World(new Vector2(0, 0), true);
+
         batch = new SpriteBatch();
         orthographicCamera = new OrthographicCamera();
         orthographicCamera.setToOrtho(false, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
@@ -22,5 +38,15 @@ public class Main extends Game {
     @Override
     public void dispose() {
         batch.dispose();
+    }
+
+    public void stepWorld() {
+        float delta = Gdx.graphics.getDeltaTime();
+        accumulator += delta;
+
+        if (accumulator >= STEP_TIME) {
+            accumulator -= STEP_TIME;
+            world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        }
     }
 }
--- core/src/main/java/ru/innovationcampus/spacecleaner/screens/GameScreen.java
+++ core/src/main/java/ru/innovationcampus/spacecleaner/screens/GameScreen.java
@@ -1,13 +1,64 @@
 package ru.innovationcampus.spacecleaner.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
 
+import ru.innovationcampus.spacecleaner.GameResources;
+import ru.innovationcampus.spacecleaner.GameSettings;
 import ru.innovationcampus.spacecleaner.Main;
+import ru.innovationcampus.spacecleaner.objects.ShipObject;
 
 public class GameScreen extends ScreenAdapter {
     Main main;
+    ShipObject shipObject;
 
     public GameScreen(Main main) {
         this.main = main;
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        shipObject = new ShipObject(GameSettings.SCREEN_WIDTH / 2, 150,
+            GameSettings.SHIP_WIDTH, GameSettings.SHIP_HEIGHT,
+            GameResources.SHIP_IMG_PATH, main.world);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        main.stepWorld();
+        handleInput();
+
+        draw();
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isTouched()) {
+            main.touch = main.orthographicCamera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+            shipObject.move(main.touch);
+        }
+    }
+
+    private void draw() {
+        main.orthographicCamera.update();
+        main.batch.setProjectionMatrix(main.orthographicCamera.combined);
+        ScreenUtils.clear(Color.CLEAR);
+
+        main.batch.begin();
+        shipObject.draw(main.batch);
+        main.batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+
+        shipObject.dispose();
     }
 }
